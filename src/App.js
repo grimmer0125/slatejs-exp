76495be5bdfa95370df8b8fd7a3cae21b27b342d
Grimmer <grimmer0125@gmail.com>
2018-07-13 17:51:37 +0800

    Add first slate editor example

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
 import logo from './logo.svg';
 import './App.css';
 
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A line of text in a paragraph.',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+})
+
 class App extends Component {
+  // Set the initial value when the app is first constructed.
+  state = {
+    value: initialValue,
+  }
+
+  // On change, update the app's React state with the new editor value.
+  onChange = ({ value }) => {
+    this.setState({ value })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <Editor value={this.state.value} onChange={this.onChange} />
   }
 }
 
